454ecb7572e83bb2d8f1d4e29aca628ef04e5b49
Git <reircode@gmail.com>
2022-02-05 00:21:26 -0400

    commit

--- src/Components/ProductCard/ProductCard.js
+++ src/Components/ProductCard/ProductCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ProductCard = () => {
   return <div className='productcard'>
-           
+           <img class="smart-media smart-image-default lazyload-fade lazyloaded" data-src="//cdn.shopify.com/s/files/1/0667/0133/files/lastchancejan22-highres_1400x.jpg?v=1643853035" alt="Last Chance" src="//cdn.shopify.com/s/files/1/0667/0133/files/lastchancejan22-highres_1400x.jpg?v=1643853035"></img>
   </div>;
 };
 
