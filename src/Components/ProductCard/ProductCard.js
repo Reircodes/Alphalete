539f6df2c1fe30014f665ad10553bd318bdecb23
Git <reircode@gmail.com>
2022-02-05 00:17:26 -0400

    commit

---
+++ src/Components/ProductCard/ProductCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ProductCard = () => {
+  return <div className='productcard'>
+           
+  </div>;
+};
+
+export default ProductCard;
---
+++ src/Containers/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+  return <section className='header'>
+          <div className="header__container">
+              
+          </div>
+  </section>;
+};
+
+export default Header;
